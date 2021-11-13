a5d4cb105ebcb4c35d1ff586652c89fbd05295f6
Your Name <ladislavvasina@gmail.com>
2021-11-13 13:50:36 +0100

    Library for working with dynamic strings

---
+++ str.h
@@ -0,0 +1,30 @@
+/**
+ *  @file str.h
+ *
+ *  @brief Header file for str.c (Header file that enables working with dynamic strings)
+ *
+ *  @author <xvasin11> Ladislav Vasina
+ */
+
+
+/**
+ * @brief string structure
+ */
+
+typedef struct{
+    char *data;    // Var to store the string
+    int length;   // String lenght
+    int capacity; // Memory allocated
+} string;
+
+int strInit(string *s);
+void strFree(string *s);
+void strClear(string *s);
+int strAppendChar(string *s1, char toAppend);
+int strCopyString(string *s1, string *s2);
+int strCopyConstant(string *s1, char *c);
+int strCompareString(string *s1, string *s2);
+int strCompareConstant(string *s1, char *s2);
+char *strGetString(string *s);
+char strGetLenght(string *s);
+char strGetCapacity(string *s);
