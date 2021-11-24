eb7a186e243683a038d16c313e7ec49b16dfe0a4
Vojtěch Hájek <vojtech.hajek2@gmail.com>
2021-11-24 16:21:32 +0100

    update tokenList.c and tokenList.h

--- tokenList.c
+++ tokenList.c
@@ -13,35 +13,184 @@ void DLL_Init(DLList *list)
     list->first = NULL;
     list->last = NULL;
     list->active = NULL;
-};
+}
+/*DLL_Init*/
 
-void DLL_Dispose(DLList *list);
+void DLL_Dispose(DLList *list){
+    token_ptr token;
+    while (list->last != NULL){
+        token = list->last;
+        list->last = token->prev;
+        free(token);
+    }
+    DLL_Init(list);
+}
+/*DLL_Dispose*/
 
-void DLL_InsertFirst(DLList *list, token_ptr token);
+void DLL_InsertFirst(DLList *list, token_ptr token){
+    token_ptr new_token = (token_ptr) malloc(sizeof (struct token));
+    new_token = token;
+    new_token->prev = NULL;
+    new_token->next = list->first;
+    if(list->first != NULL){
+        list->first->prev = new_token;
+    } else{
+        list->last = new_token;
+    }
+    list->first = new_token;
+}
+/*DLL_InsertFirst*/
 
-void DLL_InsertLast(DLList *list, token_ptr token);
+void DLL_InsertLast(DLList *list, token_ptr token){
+    token_ptr new_token = (token_ptr) malloc(sizeof (struct token));
+    new_token = token;
+    new_token->next = NULL;
+    new_token->prev = list->last;
+    if(list->last != NULL){
+        list->last->next = new_token;
+    } else{
+        list->first = new_token;
+    }
+    list->last = new_token;
+}
+/*DLL_InsertLast*/
 
-void DLL_First(DLList *list);
+void DLL_First(DLList *list){
+    list->active = list->first;
+}
+/*DLL_First*/
 
-void DLL_Last(DLList *list);
+void DLL_Last(DLList *list){
+    list->active = list->last;
+}
+/*DLL_Last*/
 
-void DLL_GetFirst(DLList *list, token_ptr *token);
+void DLL_GetFirst(DLList *list, token_ptr *token){
+    if(list->first != NULL){
+        *token = list->first->data;
+    }
+}
+/*DLL_GetFirst*/
 
-void DLL_GetLast(DLList *list, token_ptr *token);
+void DLL_GetLast(DLList *list, token_ptr *token){
+    if(list->first != NULL){
+        *token = list->last->data;
+    }
+}
+/*DLL_GetLast*/
 
-void DLL_DeleteFirst(DLList *list);
+void DLL_DeleteFirst(DLList *list){
+    if(list->first != NULL){
+        token_ptr token;
+        token = list->first;
+        if(list->active == list->first){
+            list->active;
+        }
+        if(list->first == list->last){
+            list->first = NULL;
+            list->last = NULL;
+        } else{
+            list->first = list->first->next;
+            list->first->prev = NULL;
+        }
+        free(token);
+    }
+}
+/*DLL_DeleteFirst*/
 
-void DLL_DeleteLast(DLList *list);
+void DLL_DeleteLast(DLList *list){
+    if(list->last != NULL){
+        token_ptr token;
+        if(list->active == list->last){
+            list->active == NULL;
+        }
+        if(list->last == list->first){
+            list->last = NULL;
+            list->first = NULL;
+        } else{
+            list->last = list->last->prev;
+            list->last->next = NULL;
+        }
+        free(token);
+    }
+}
+/*DLL_DeleteLast*/
 
-void DLL_DeleteAfter(DLList *list);
+void DLL_DeleteAfter(DLList *list){
+    if(list->active != NULL){
+        token_ptr token;
+        token = list->active->next;
+        list->active->next = token->next;
+        if(token == list->last){
+            list->last = list->active;
+        } else{
+            token->next->prev = list->active;
+        }
+        free(token);
+    }
+}
+/*DLL_DeleteAfter */
 
-void DLL_DeleteBefore(DLList *list);
+void DLL_DeleteBefore(DLList *list){
+    if(list->active != NULL){
+        if(list->active->prev != NULL){
+            token_ptr token;
+            token = list->active->prev;
+            list->active->prev = token->prev;
+            if(token == list->first){
+                list->first = list->active;
+            } else{
+                token->prev->next = list->active;
+            }
+            free(token)
+        }
+    }
+}
+/*DLL_DeleteBefore*/
 
-void DLL_InsertAfter(DLList *list, token_ptr token);
+void DLL_InsertAfter(DLList *list, token_ptr token){
+    if(list->active != NULL){
+        token_ptr new_token = (token_ptr) malloc(sizeof(struct token));
 
-void DLL_InsertBefore(DLList *list, token_ptr token);
+        new_token = token;
+        new_token->next = list->active->next;
+        new_token->prev = list->active;
+        list->active->next = new_token;
 
-void DLL_GetValue(DLList *list, token_ptr *token);
+        if(list->active == list->last){
+            list->last = new_token;
+        } else{
+            new_token->next->prev = new_token;
+        }
+    }
+}
+/*DLL_InsertAfter */
+
+void DLL_InsertBefore(DLList *list, token_ptr token){
+    if(list->active != NULL){
+        token_ptr new_token = (token_ptr) malloc(sizeof(struct token));
+
+        new_token = token;
+        new_token->prev = list->active->prev;
+        new_token->next = list->active;
+        list->active->prev = new_token;
+
+        if(list->active == list->first){
+            list->first = new_token;
+        }
+        else{
+            new_token->prev->next = new_token;
+        }
+    }
+}
+/*DLL_InsertBefore*/
+
+void DLL_GetValue(DLList *list, token_ptr *token){
+    if(list->active != NULL){
+        *token = list->active->data;
+    }
+}
+/*DLL_GetValue*/
 
 void DLL_SetValue(DLList *list, token_ptr token)
 {
--- tokenList.h
+++ tokenList.h
@@ -11,6 +11,7 @@
 #include "scanner.h"
 #include "error.h"
 
+
 /**
  * @brief Doubly linked list of tokens
  */
@@ -27,61 +28,101 @@ typedef struct {
 void DLL_Init(DLList *list);
 
 /**
- * @brief List initialization
+ * @brief List dispose
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_Dispose(DLList *list);
 
+/**
+ * @brief Inserts new element at the beginning of the list
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Value to insert at the beginning of the list
+ */
 void DLL_InsertFirst(DLList *list, token_ptr token);
 
+/**
+ * @brief Inserts new element at the end of the list
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Value to insert at the end of the list
+ */
 void DLL_InsertLast(DLList *list, token_ptr token);
 
 /**
- * @brief List initialization
+ * @brief Sets first list item to active
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_First(DLList *list );
 
 /**
- * @brief List initialization
+ * @brief Sets last list item to active
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_Last(DLList *list );
 
+/**
+ * @brief The token parameter returns the value of the first list item
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Pointer to target variable
+ */
 void DLL_GetFirst(DLList *list, token_ptr *token);
 
+/**
+ * @brief The token parameter returns the value of the last list item
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Pointer to target variable
+ */
 void DLL_GetLast(DLList *list, token_ptr *token);
 
 /**
- * @brief List initialization
+ * @brief Deletes first element in the list
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_DeleteFirst(DLList *list);
 
 /**
- * @brief List initialization
+ * @brief Deletes last element in the list
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_DeleteLast(DLList *list);
 
 /**
- * @brief List initialization
+ * @brief Deletes an element after the active item list
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_DeleteAfter(DLList *list);
 
 /**
- * @brief List initialization
+ * @brief Deletes an element before the active item list
  * @param list Pointer to the structure of a doubly linked list
  */
 void DLL_DeleteBefore(DLList *list);
 
+/**
+ * @brief Insert an element after active item list
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Value to insert into the list after the currently active element
+ */
 void DLL_InsertAfter(DLList *list, token_ptr token);
 
+/**
+ * @brief Insert an element before active item of list
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Value to insert into the list before the currently active element
+ */
 void DLL_InsertBefore(DLList *list, token_ptr token);
 
+/**
+ * @brief The token parameter returns the value of the active list item
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token Pointer to target variable
+ */
 void DLL_GetValue(DLList *list, token_ptr *token);
 
+/**
+ * @brief Overwrites the contents of the active list item.
+ * @param list Pointer to the structure of a doubly linked list
+ * @param token New value of active item
+ */
 void DLL_SetValue(DLList *list, token_ptr token);
 
 /**
