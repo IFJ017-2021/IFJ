0f3cbfc0da6a4d32210f0fde43e778cdd268d558
Ladislav Vašina <ladislavvasina@gmail.com>
2021-11-26 21:20:14 +0100

    precedence table created

---
+++ prec_table.c
@@ -0,0 +1,35 @@
+/**
+ *  @file prec_table.c
+ *
+ *  @brief Implementation of precedence table
+ *
+ *  @author <xvasin11> Ladislav Vasina
+ */
+
+#include "prec_table.h"
+#include "error.h"
+#include "parser.h"
+
+#define TABLE_SIZE 17
+
+int precedence_table[TABLE_SIZE][TABLE_SIZE] =
+{
+//         |+ |-| *| /|//| <| >|<=|>=|~=|==| (| )| i| #|..| $|
+/* + */    {R, R, S, S, S, R, R, R, R, R, R, S, R, S, S, R, R},
+/* - */    {R, R, S, S, S, R, R, R, R, R, R, S, R, S, S, R, R},
+/* * */    {R, R, R, R, R, R, R, R, R, R, R, S, R, S, S, R, R},
+/* / */    {R, R, R, R, R, R, R, R, R, R, R, S, R, S, S, R, R},
+/* //*/    {R, R, R, R, R, R, R, R, R, R, R, S, R, S, S, R, R},
+/* < */    {S, S, S, S, S, R, R, R, R, R, R, S, R, S, S, S, R},
+/* > */    {S, S, S, S, S, R, R, R, R, R, R, S, R, S, S, S, R},
+/* <=*/    {S, S, S, S, S, R, R, R, R, R, R, S, R, S, S, S, R},
+/* >=*/    {S, S, S, S, S, R, R, R, R, R, R, S, R, S, S, S, R},
+/* ~=*/    {S, S, S, S, S, S, S, S, S, R, R, S, R, S, S, S, R},
+/* ==*/    {S, S, S, S, S, S, S, S, S, R, R, S, R, S, S, S, R},
+/* ( */    {S, S, S, S, S, S, S, S, S, S, S, S, E, S, S, S, X},
+/* ) */    {R, R, R, R, R, R, R, R, R, R, R, X, R, X, X, R, R},
+/* i */    {R, R, R, R, R, R, R, R, R, R, R, X, R, X, X, R, R},
+/* # */    {R, R, R, R, R, R, R, R, R, R, R, S, R, S, S, R, R},
+/* ..*/    {S, S, S, S, S, R, R, R, R, R, R, S, R, S, S, S, R},
+/* $ */    {S, S, S, S, S, S, S, S, S, S, S, S, X, S, S, S, X},
+}
