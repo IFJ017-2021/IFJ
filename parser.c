3cc1ba6499563f00162d23fbb422626424a7349e
Vojtěch Hájek <vojtech.hajek2@gmail.com>
2021-12-01 14:16:30 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -26,14 +26,13 @@
   if (token->type != (_type))                                                  \
   err_call(ERR_SYNTAX)
 
-#define IS_TYPE_VALUE()                                                  \
-  if ((_token->type != T_DOUBLE) && (_token->type != T_INT) &&                 \
-      (_token->type != T_STRING) && (_token->type != T_ID))                    \
+#define IS_TYPE_VALUE()                                                         \
+  if ((token->type != T_K_INTEGER) && (token->type != T_K_STRING) &&            \
+      (token->type != T_K_NUMBER) && (token->type != T_K_NIL))                  \
   err_call(ERR_SYNTAX)
 
 #define  IS_EXPRESSION() \
-    if((token->type == T_ID)          \
-    && (token->next->type != T_ASSIGN)  \
+    if((token->next->type != T_ASSIGN)  \
     && (token->next->type != T_EQL)     \
     && (token->next->type != T_GT)      \
     && (token->next->type != T_GTE)     \
@@ -46,7 +45,10 @@
     && (token->next->type != T_DIV)     \
     && (token->next->type != T_IDIV)    \
     && (token->next->type != T_STRLEN)  \
-    ){}else {/*expression(); */}
+    ){return;}                          \
+    else{                               \
+        GET_TOKEN()                     \
+    }
 
 DLList token_list;
 token_ptr token;
@@ -62,10 +64,9 @@ void start(DLList *testlist) {
   //    {
   //        err_call(ERR_LEX);
   //    }
-  GET_TOKEN()
   CHECK_TYPE(T_K_REQUIRE);
   GET_TOKEN()
-  if (token->type != T_STRING && strcmp(token->data->string, "ifj21")) {
+  if (token->type != T_STRING && strcmp(token->data->string, "ifj21") != 0) {
     err_call(ERR_SYNTAX);
   }
   GET_TOKEN()
@@ -312,7 +313,7 @@ void entry_list_params(){
   }
 }
 void entry_param() {
-IS_EXPRESSION()
+//IS_EXPRESSION();
 }
 
 void entry_param_next() {
@@ -359,7 +360,7 @@ void type_next() {
     DLL_Previous(&token_list);
   }
 }
-void type_value() { IS_TYPE_VALUE(token); }
+void type_value() { IS_TYPE_VALUE(); }
 void return_list() {
   if (token->type == T_ID) {
     GET_TOKEN()
@@ -397,15 +398,50 @@ void state_else() {
 }
 void init_value() {
   if (/*expression*/ (token->type == T_ID) || (token->type == T_INT) ||
-      (token->type == T_DOUBLE) || (token->type == T_STRING)) {
-    GET_TOKEN()
-    CHECK_TYPE(T_LEFT_PAR);
+  (token->type == T_DOUBLE) || (token->type == T_STRING)) {
+    if((token->type == T_ID)
+    && (token->next->type != T_ASSIGN)
+    && (token->next->type != T_EQL)
+    && (token->next->type != T_GT)
+    && (token->next->type != T_GTE)
+    && (token->next->type != T_LT)
+    && (token->next->type != T_LTE)
+    && (token->next->type != T_NEQL)
+    && (token->next->type != T_MUL)
+    && (token->next->type != T_SUB)
+    && (token->next->type != T_ADD)
+    && (token->next->type != T_DIV)
+    && (token->next->type != T_IDIV)
+    && (token->next->type != T_STRLEN)){
+          GET_TOKEN()
+          CHECK_TYPE(T_LEFT_PAR);
 
-    GET_TOKEN()
-    entry_list_params();
+          GET_TOKEN()
+          entry_list_params();
 
-    GET_TOKEN()
-    CHECK_TYPE(T_RIGHT_PAR);
+          GET_TOKEN()
+          CHECK_TYPE(T_RIGHT_PAR);
+    } else {
+        while (token->next->type != T_EOF){
+            switch (token->type) {
+                case T_ID:
+                    IS_EXPRESSION();
+                    break;
+                case T_INT:
+                    IS_EXPRESSION();
+                    break;
+                case T_DOUBLE:
+                    IS_EXPRESSION();
+                    break;
+                case T_STRING:
+                    IS_EXPRESSION();
+                    break;
+                default:
+                    GET_TOKEN()
+                    break;
+            }
+        }
+    }
   } else {
     DLL_Previous(&token_list);
   }
