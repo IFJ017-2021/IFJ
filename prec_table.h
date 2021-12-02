4ea0273de5ec9c332d945a38b1b430ea5f03d44c
Ladislav Vašina <ladislavvasina@gmail.com>
2021-12-02 17:14:11 +0100

    rules enum added

--- prec_table.h
+++ prec_table.h
@@ -19,7 +19,6 @@ typedef enum{
 
 /**
  * @enum Precedence table symbols
- * 
  */
 typedef enum{
     PLUS,           // +
@@ -36,7 +35,26 @@ typedef enum{
     LEFT_BRACKET,   // (
     RIGHT_BRACKET,  // )
     ID,             // i
-    LENGHT,         // #
+    LENGTH,         // #
     CONCAT,         // ..
     END_DOLLAR      // $
 } prec_table_symbols;
+
+/**
+ * @enum Parsing rules
+ */
+typedef enum{
+    R_PLUS,         // E -> E + E 0
+    R_MINUS,        // E-> E - E 1
+    R_MUL,          // E -> E * E 2
+    R_DIV,          // E -> E / E 3
+    R_EQUAL,        // E -> E = E 5
+    R_NEQUAL,       // E -> E != E 6
+    R_MOE,          // E -> E >= E 7
+    R_LOE,          // E -> E <= E 8
+    R_LESS,         // E -> E < E 9
+    R_MORE,         // E -> E > E 10
+    R_EBRACES,      // E -> (E) 11
+    R_OP,           // E -> I 12
+    R_NOTDEFINED    // NONDEFINED RULE 13
+} prec_parsing_rules;
